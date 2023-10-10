8ceda3f679a69f4e6a0fcbaedffa45bb879bd8f0
GitHubのユーザ名 <e7a21078b0@edu.teu.ac.jp>
2023-10-11 03:55:03 +0900

    ブログ投稿の削除

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -45,6 +45,10 @@ class Postcontroller extends Controller
        return redirect('/posts/' . $post->id);
    }
   
-
+   public function delete(Post $post)
+   {
+    $post->delete();
+    return redirect('/');
+   }
 }
 ?>
---
+++ resources/posts/index.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+    　　 <title>Blog</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+      
+    </head>
+    <body class="antialiased">
+        <h1>Blog Name</h1>
+        <a href='/posts/create'>create</a>
+        <div Class= 'posts'>
+            @foreach($posts as $post)
+            <div class='post'>
+                <a href="/posts/{{ $post->id}}"><h2 class='title'>{{ $post->title }}</h2></a>
+                <p class='body'>{{ $post->body }}</p>
+                @csrf
+                @method('DELETE')
+                <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                <button type="button" onclick="deletePost({{ $post->id }})">delete</button> 
+            </div>
+            @endforeach
+        </div>
+        <div class='paginate'>{{ $posts->links()}}</div>
+        <script>
+        function deletePost(id) {
+        'use strict'
+
+        if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+            document.getElementById(`form_${id}`).submit();
+        }
+    }
+</script>
+    </body>
+</html>
